abd382afc7caeaaf08661e06e23948aa8e27fd21
Mirza-Anas <mirzanas194@gmail.com>
2024-01-04 16:56:04 +0530

    responsive1

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,3 +1,12 @@
 $(() => {
-    console.log("page loaded")
-})
+    const menu = $(".header-navbar-portrait img");
+    const submenu = $(".header-navbar-submenu");
+
+    menu.click(() => {
+        if (submenu.css("display") == "none") {
+            submenu.css({ display: "block" });
+        } else {
+            submenu.css({ display: "none" });
+        }
+    });
+});
